49b28a58a89586f4e0d86ff17062c091d0b17ca8
Pedro <pedro@eletrocenterlondrina.com.br>
2023-08-01 11:15:35 -0300

    Criando Desconto Extra no Orçamento

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,6 +6,7 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidade) {
         this.valor = valor;
@@ -18,5 +19,21 @@ public class Orcamento {
 
     public int getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    // codigo ficou cheio de if e else
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+        if (situacao.equals("EM_ANALISE")) {
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        } else if (situacao.equals("APROVADO")) {
+            valorDoDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    // transicao de estado não esta legal, podendo ir e vir a qualquer momento
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 }
