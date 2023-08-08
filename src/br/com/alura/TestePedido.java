c422f3221e62388c535be0dbdbeab3ca150ceda8
Pedro <pedro@eletrocenterlondrina.com.br>
2023-08-08 11:06:58 -0300

    #5 - DISIGN PATTEERNS , OBSERVER

--- src/br/com/alura/TestePedido.java
+++ src/br/com/alura/TestePedido.java
@@ -1,20 +1,25 @@
 package br.com.alura;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandlers;
+import br.com.alura.loja.pedido.acao.EnviaEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvaPedidoNoBancoDeDados;
 
 public class TestePedido {
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "Rodrigo";
+        BigDecimal valorOrcamento = new BigDecimal("300");
+        int quantidadeItens = Integer.parseInt("2");
         // MAIN REPRESENTACAO INTERFACE COM USUARIO, POR LINHA DE COMANDO, dados vindo
         // de maneira diferente do modelo anterior
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens); // separando os dados
-        GeraPedidoHandlers handlers = new GeraPedidoHandlers(/* As dependencias externas */); // separando as
-                                                                                              // dependencias
+        GeraPedidoHandlers handlers = new GeraPedidoHandlers(
+                Arrays.asList(new SalvaPedidoNoBancoDeDados(), new EnviaEmailPedido())); // separando as dependencias
+
         handlers.executa(gerador);
     }
 }
