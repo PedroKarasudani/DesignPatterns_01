89b26362a51ae3b3190c575341568b7a18b4fd8c
Pedro <pedro@eletrocenterlondrina.com.br>
2023-08-11 14:29:35 -0300

    #7 DESIGN PATTERNS - ESTRUTURAIS( ADAPTER)

--- src/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
+++ src/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -1,8 +1,26 @@
 package br.com.alura.loja.orcamento;
 
+import java.util.Map;
+
+import br.com.alura.loja.DomainException;
+import br.com.alura.loja.http.HttpAdapter;
+
 public class RegistroDeOrcamento {
-    // chamada HTTP para a API externa
-    // URL Connection
-    // HTTP Client
-    // lib Rest
+
+    private HttpAdapter http;
+
+    public RegistroDeOrcamento(HttpAdapter http) {
+        this.http = http;
+    }
+
+    public void registrar(Orcamento orcamento) {
+        if (!orcamento.isFinalizado()) {
+            throw new DomainException("Orcamento nao finalizado!!");
+        }
+        String url = "http://api.externa/orcamento";
+        Map<String, Object> dados = Map.of(
+                "valor", orcamento.getValor(),
+                "quantidadeItens", orcamento.getQuantidadeItens());
+        http.post(url, dados);
+    }
 }
