7395e410e3f6259923fff114505f28183d7d7187
Pedro <pedro@eletrocenterlondrina.com.br>
2023-08-18 13:23:47 -0300

    #9 DESIGN PATTERNS - ESTRUTURAIS (COMPOSITE)

--- src/br/com/alura/TesteDesconto.java
+++ src/br/com/alura/TesteDesconto.java
@@ -8,12 +8,11 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 public class TesteDesconto {
     public static void main(String[] args) {
-        ItemOrcamento banana = new ItemOrcamento(new BigDecimal("100"));
-        ItemOrcamento uva = new ItemOrcamento(new BigDecimal("200"));
         Orcamento primeiroOrcamento = new Orcamento();
         Orcamento segundoOrcamento = new Orcamento();
-        primeiroOrcamento.adicionarItem(banana);
-        segundoOrcamento.adicionarItem(uva);
+        primeiroOrcamento.adicionarItem(new ItemOrcamento(new BigDecimal("100")));
+        segundoOrcamento.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
+
         CalculadoraDeDesconto valorDesconto = new CalculadoraDeDesconto();
 
         System.out.println(valorDesconto.calcular(primeiroOrcamento));
